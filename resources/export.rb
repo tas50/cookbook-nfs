678531f25e8b3850412f9f8a0ffe041381733ba8
Tim Smith <tsmith@chef.io>
2018-05-03 17:16:29 -0700

    Convert to a custom resource
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- resources/export.rb
+++ resources/export.rb
@@ -3,6 +3,7 @@
 # Resources:: export
 #
 # Copyright 2012, Riot Games
+# Copyright 2018, Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,18 +18,103 @@
 # limitations under the License.
 #
 
-def initialize(*args)
-  super
-  @action = :create
+property :directory, property: true
+property :network, required: true
+property :writeable, [TrueClass, FalseClass], default: false
+property :sync, [TrueClass, FalseClass], default: true
+property :options, Array, default: ['root_squash']
+property :anonuser
+property :anongroup
+property :unique, [TrueClass, FalseClass], default: false
+
+action :create do
+  sub_run_context = @run_context.dup
+  sub_run_context.resource_collection = Chef::ResourceCollection.new
+
+  begin
+    original_run_context = @run_context
+    @run_context = sub_run_context
+
+    ro_rw = new_resource.writeable ? 'rw' : 'ro'
+    sync_async = new_resource.sync ? 'sync' : 'async'
+    options = new_resource.options.join(',')
+    options = ",#{options}" unless options.empty?
+    options << ",anonuid=#{find_uid(new_resource.anonuser)}" if new_resource.anonuser
+    options << ",anongid=#{find_gid(new_resource.anongroup)}" if new_resource.anongroup
+
+    if new_resource.network.is_a?(Array)
+      host_permissions = new_resource.network.map { |net| net + "(#{ro_rw},#{sync_async}#{options})" }
+      export_line = "#{new_resource.directory} #{host_permissions.join(' ')}\n"
+    else
+      export_line = "#{new_resource.directory} #{new_resource.network}(#{ro_rw},#{sync_async}#{options})\n"
+    end
+
+    execute 'exportfs' do
+      command 'exportfs -ar'
+      action :nothing
+    end
+
+    if ::File.zero?('/etc/exports') || !::File.exist?('/etc/exports')
+      file '/etc/exports' do
+        content export_line
+        notifies :run, 'execute[exportfs]', :immediately
+      end
+    elsif new_resource.unique
+      replace_or_add "export #{new_resource.name}" do
+        path '/etc/exports'
+        pattern "^#{new_resource.directory} "
+        line export_line
+        notifies :run, 'execute[exportfs]', :immediately
+      end
+    else
+      append_if_no_line "export #{new_resource.name}" do
+        path '/etc/exports'
+        line export_line
+        notifies :run, 'execute[exportfs]', :immediately
+      end
+    end
+  ensure
+    @run_context = original_run_context
+  end
+
+  # converge
+  begin
+    Chef::Runner.new(sub_run_context).converge
+  ensure
+    if sub_run_context.resource_collection.any?(&:updated?)
+      new_resource.updated_by_last_action(true)
+    end
+  end
 end
 
-actions :create
+action_class do
+  # Finds the UID for the given user name
+  #
+  # @param [String] username
+  # @return
+  def find_uid(username)
+    uid = nil
+    Etc.passwd do |entry|
+      if entry.name == username
+        uid = entry.uid
+        break
+      end
+    end
+    uid
+  end
 
-attribute :directory, :name_attribute => true
-attribute :network, :required => true
-attribute :writeable, :default => false, :kind_of => [TrueClass, FalseClass]
-attribute :sync, :default => true, :kind_of => [TrueClass, FalseClass]
-attribute :options, :default => ['root_squash'], :kind_of => Array
-attribute :anonuser
-attribute :anongroup
-attribute :unique, :default => false, :kind_of => [TrueClass, FalseClass]
+  # Finds the GID for the given group name
+  #
+  # @param [String] groupname
+  # @return [Integer] the matching GID or nil
+  def find_gid(groupname)
+    gid = nil
+    Etc.group do |entry|
+      if entry.name == groupname
+        gid = entry.gid
+        break
+      end
+    end
+    gid
+  end
+end
