36c04599e6eed120639e6e6a15a6612d28183895
Tim Smith <tsmith@chef.io>
2018-05-03 17:17:13 -0700

    Require Chef 12.7 and Chef 11-isms
    action_class didn't really work right until 12.7
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -5,9 +5,9 @@ description 'Installs and configures NFS, and NFS exports'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 name 'nfs'
 version '2.5.1'
-source_url 'https://github.com/atomic-penguin/cookbook-nfs' if respond_to?(:source_url)
-issues_url 'https://github.com/atomic-penguin/cookbook-nfs/issues' if respond_to?(:issues_url)
-chef_version '>= 12.5' if respond_to?(:chef_version)
+source_url 'https://github.com/atomic-penguin/cookbook-nfs'
+issues_url 'https://github.com/atomic-penguin/cookbook-nfs/issues'
+chef_version '>= 12.7' if respond_to?(:chef_version)
 
 supports 'ubuntu', '>= 12.04'
 supports 'debian', '>= 8.0'
